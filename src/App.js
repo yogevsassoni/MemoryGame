3281472fd3dd6b734e3cee151bfd07832f95b30f
yoad sassoni <yogevsas@gmail.com>
2025-06-29 15:27:52 +0300

    added restart game and move counter features

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ function shuffleArray(array) {
 
 function App() {
   const values = ["🐶", "🐱", "🐼", "🦊", "🐵", "🐸", "🐷", "🐥"];
-  const [cards] = useState(() => shuffleArray([...values, ...values]));
+  const [cards, setCards] = useState(() => shuffleArray([...values, ...values]));
   const [flipped, setFlipped] = useState([]);
   const [matchedCard, setMatchedCard] = useState([]);
+  const [movesCount, setMovesCount] = useState(0);
+  const [pairFound, setPairFound] = useState(0);
 
   const handleClick = (index) => {
     if (flipped.length < 2 && !flipped.includes(index) && !matchedCard.includes(index)) {
@@ -29,14 +31,34 @@ function App() {
       const firstValue = cards[firstIndex];
       const secondValue = cards[secondIndex];
 
+      setMovesCount(prev => prev + 1);
+
       if (firstValue === secondValue) {
         setMatchedCard(prev => [...prev, firstIndex, secondIndex]);
+        setPairFound(prev => prev + 1);
         setFlipped([]); 
       } else {
-        setFlipped([]);
+        const timeout = setTimeout(() => {
+          setFlipped([]);
+        }, 1000);
+        return () => clearTimeout(timeout);
       }
     }
-  }, [flipped, cards]);
+  }, [flipped,matchedCard,cards]);
+
+  useEffect(() => {
+  if (pairFound === values.length) {
+    alert("Victory!");
+  }
+  }, [pairFound, values.length]);
+
+
+  const restartGame = () => {
+    setMatchedCard([]);
+    setMovesCount(0);
+    setPairFound(0);
+    setCards(() => shuffleArray([...values, ...values]));
+  }
 
   const gridStyle = {
     display: 'grid',
@@ -47,16 +69,27 @@ function App() {
     padding: '20px',
   };
 
+
+
   return (
-    <div className='card-container' style={gridStyle}>
-      {cards.map((value, index) => (
-        <Card
-          key={index}
-          value={value}
-          isFlipped={flipped.includes(index) || matchedCard.includes(index)}
-          onClick={() => handleClick(index)}
-        />
-      ))}
+    <div>
+      <p style={{textAlign:'center'}}>
+        moves done: {movesCount}
+      </p>
+      {pairFound === values.length && <button onClick={restartGame} style={{textAlign:'center'}}>
+        restart game!
+      </button>
+      }
+      <div className='card-container' style={gridStyle}>
+        {cards.map((value, index) => (
+          <Card
+            key={index}
+            value={value}
+            isFlipped={flipped.includes(index) || matchedCard.includes(index)}
+            onClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
