2d577664012b4a1569977026f5455c25b10d841c
yoad sassoni <yogevsas@gmail.com>
2025-06-29 12:22:10 +0300

    showing all of the cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+
+function shuffleArray(array) {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
 
 function App() {
+  const values = ["🐶", "🐱", "🐼", "🦊", "🐵", "🐸", "🐷", "🐥"];
+  let cards = [...values, ...values];
+  cards = shuffleArray(cards);
+  
+  console.log(cards);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card-container'>
+      {cards.map(card => <Card value={card} isFlipped={true} />)}  
     </div>
-  );
+    );
 }
 
 export default App;
